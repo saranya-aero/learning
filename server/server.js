68859eb4f336a1b8c3a4f59f7e6735067f04f9fa
Saranya <saranaero90@gmail.com>
2017-05-21 23:25:57 +0530

    Changes for socket

--- server/server.js
+++ server/server.js
@@ -1,15 +1,23 @@
 const path = require('path');
 const publicPath =  path.join(__dirname, '../public');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const port = process.env.PORT || 8000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
 console.log(publicPath);
 //console.log()
 
 app.use(express.static(publicPath));
 
-
-app.listen(port, () =>{
-  console.log(`Server is open at ${port}:`);
+io.on('connection', (socket) => {
+  console.log("New user connected");
 });
+
+ server.listen(8000, () =>{
+   console.log(`Server is open at ${port}:`);
+ });
